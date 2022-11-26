c03845687234548a112317ea9721e106d996480d
André Pereira <andrelcop@gmail.com>
2022-11-26 20:32:02 +0100

    create  4 components: favourites, details, login, select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectComponent } from './select/select.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { DetailsComponent } from './details/details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent,
+    FavoritesComponent,
+    DetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
