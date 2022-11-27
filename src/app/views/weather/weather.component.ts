b4abc31f4da7da39120dcfd4a7b992d321939aec
André Pereira <andrelcop@gmail.com>
2022-11-27 19:54:11 +0100

    clean code

--- src/app/views/weather/weather.component.ts
+++ src/app/views/weather/weather.component.ts
@@ -14,16 +14,16 @@ export class WeatherComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.cityShowed = {
-      "id": 2509300,
-      "name": "Zújar",
-      "state": "",
-      "country": "ES",
-      "coord": {
-        "lon": -2.84197,
-        "lat": 37.54285
-      }
-    };
+    // this.cityShowed = {
+    //   "id": 2509300,
+    //   "name": "Zújar",
+    //   "state": "",
+    //   "country": "ES",
+    //   "coord": {
+    //     "lon": -2.84197,
+    //     "lat": 37.54285
+    //   }
+    // };
   }
   citySelected(city: City) {
     console.log('City: ', city);
