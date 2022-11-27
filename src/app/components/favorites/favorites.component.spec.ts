3df037f88a16649ac67b40191d4b86149c626462
André Pereira <andrelcop@gmail.com>
2022-11-28 00:52:54 +0100

    add tests for favourites component

--- src/app/components/favorites/favorites.component.spec.ts
+++ src/app/components/favorites/favorites.component.spec.ts
@@ -22,4 +22,87 @@ describe('FavoritesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('cityChanged method call citySelectedEvent eventEmitter', () => {
+    let eventEmitterSpy = spyOn(component.citySelectedEvent, 'emit');
+    component.favouritesCities = [{
+      "id": 2509300,
+      "name": "Zújar",
+      "state": "",
+      "country": "ES",
+      "coord": {
+        "lon": -2.84197,
+        "lat": 37.54285
+      }
+    },
+    {
+      "id": 2509302,
+      "name": "Zuheros",
+      "state": "",
+      "country": "ES",
+      "coord": {
+        "lon": -4.31531,
+        "lat": 37.54332
+      }
+    },
+    {
+      "id": 2509304,
+      "name": "Zufre",
+      "state": "",
+      "country": "ES",
+      "coord": {
+        "lon": -6.33333,
+        "lat": 37.833328
+      }
+    }];
+
+    component.cityChanged([2509304]);
+    expect(eventEmitterSpy).toHaveBeenCalled();
+  })
+  it('cityChanged method call citySelectedEvent eventEmitter', () => {
+    let favouriteRemovedSpy = spyOn(component.favouriteRemoved, 'emit');
+    let favouritesCitiesChangeSpy = spyOn(component.favouritesCitiesChange, 'emit');
+    component.favouritesCities = [{
+      "id": 2509300,
+      "name": "Zújar",
+      "state": "",
+      "country": "ES",
+      "coord": {
+        "lon": -2.84197,
+        "lat": 37.54285
+      }
+    },
+    {
+      "id": 2509302,
+      "name": "Zuheros",
+      "state": "",
+      "country": "ES",
+      "coord": {
+        "lon": -4.31531,
+        "lat": 37.54332
+      }
+    },
+    {
+      "id": 2509304,
+      "name": "Zufre",
+      "state": "",
+      "country": "ES",
+      "coord": {
+        "lon": -6.33333,
+        "lat": 37.833328
+      }
+    }];
+    let city = {
+      "id": 2509304,
+      "name": "Zufre",
+      "state": "",
+      "country": "ES",
+      "coord": {
+        "lon": -6.33333,
+        "lat": 37.833328
+      }
+    };
+    component.deleteFavourite(city);
+    expect(favouriteRemovedSpy).toHaveBeenCalled();
+    expect(favouritesCitiesChangeSpy).toHaveBeenCalled();
+  })
 });
